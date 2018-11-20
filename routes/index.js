aa6c6ad26508ed0cc7627a2253e569f2dd294cec
hrk <hrkbrahmbhatt@gmail.com>
2018-11-21 02:36:00 +0530

    project structure + login,register UX

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('home', {title: 'Express'});
+});
+
+router.get('/home', function(req, res) {
+    res.render('home', {title: 'Express'});
+});
+router.get('/login', function(req, res) {
+    res.render('login', {title: 'Express'});
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+
+const routes = require('./routes/index')
+
+
+var app = express();
+
+
+//all environment 
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+app.use(routes);
+
+
+
+//get route from public directory
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+    console.log('Express Server listening on port' + app.get('port'));
+
+});
+
