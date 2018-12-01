db1b2439508ee3449ade1d7b9486111a27d5a74a
Haythem Sellami <sellamihaythem96@gmail.com>
2018-12-01 15:37:40 +0100

    move JS script into /src

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views' + '/');
 
-
 app.use(routes);
 
 
 
 //get route from public directory
-
 app.use(express.static(path.join(__dirname,'public')));
+//access files inside src
+app.use(express.static(__dirname + '/src'));
 
 
 var server = http.createServer(app).listen(app.get('port'),function(){
