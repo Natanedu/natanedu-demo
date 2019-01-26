f397d2dd48c9ea49590fa9cdf40786e7dba7ece4
Carlos Fuentes <metcdoder95@MacBook-Pro-de-Carlos.local>
2019-01-26 00:32:32 -0800

    Conferences socket namespace added

--- socket_server/index.js
+++ socket_server/index.js
@@ -2,6 +2,7 @@ const Socket_Server = require("socket.io");
 
 const Teachers_Socket = require("./teacher");
 const Students_Socket = require("./student");
+const Conferences_Socket = require("./conferences");
 
 module.exports = (url, app) => {
   const io = app ? new Socket_Server(app) : new Socket_Server(3030);
@@ -11,11 +12,11 @@ module.exports = (url, app) => {
       3000}`
   );
 
-  console.log(url);
-
   const Teacher_Nsp = io.of("/teachers");
   const Default_Nsp = io.of("/students");
+  const Conferences_Nsp = io.of("/conferences");
 
   Teachers_Socket(Teacher_Nsp, Default_Nsp, { url });
   Students_Socket(Default_Nsp, Teacher_Nsp, { url });
+  Conferences_Socket(Conferences_Nsp);
 };
