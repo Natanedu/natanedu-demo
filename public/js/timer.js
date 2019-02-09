6e3fceeebdcb628c22612ab0f5928e4af5fd0662
liodali <lionelviperdali@gmail.com>
2019-02-09 01:37:15 +0100

    record call,fix timer call

--- public/js/timer.js
+++ public/js/timer.js
@@ -2,24 +2,16 @@
 
 
 function caluclate() {
-    // Get todays date and time
-    var now = new Date().getTime();
+    var minutes = parseInt(counterdown / 60, 10);
+    var seconds = parseInt(counterdown % 60, 10);
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    if (distance > 0) {
-        // Display the result in the element with id="demo"
-        document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
-        console.log(minutes + "m " + seconds + "s ");
-    }
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+    console.log(minutes + "m " + seconds + "s ");
+    
     // If the count down is finished, write some text 
-    if (distance == 0) {
+    if (--counterdown <= 0) {
         connection.send({ course: "finish" })
         connection.send({ msg: "course is finish!", type: "finish" })
         // stop all local cameras
